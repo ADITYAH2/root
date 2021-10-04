161b31118b1346a3b13cde779abb342b76600cdd
muhammad amin <87260224+ADITYAH2@users.noreply.github.com>
2021-10-04 08:38:15 +0800

    Add files via upload

--- root.sh
+++ root.sh
@@ -11,22 +11,22 @@ read -p "Please Input an New Password For Root : " Pass
 
 # Starting Changes To Root
 rm -f /etc/ssh/sshd_config
-wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/hesoyam-oke/root/main/sshd
+wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/ADITYAH2/root/main/sshd
 echo -e "$Pass\n$Pass\n"|passwd root &> /dev/null
 IPANDA=$(wget -qO- ifconfig.co);
 
 # Successful
 clear
-echo "Please Save This VPS Account Information"
-echo "============================================"
+echo "Please Save This VPS Account Information" | lolcat
+echo "============================================" lolcat
 echo "  IP ADDRESS = $IPANDA"
 echo "  Username   = root"
 echo "  Password   = $Pass"
-echo "============================================"
+echo "============================================" lolcat
 echo ""
-echo "VPS Will Rebooting On 5 Second !!!!!"
+#echo "VPS Will Rebooting On 5 Second !!!!!"
 
-rm -f root.sh
-sleep 5
-reboot
+#rm -f root.sh
+#sleep 5
+#reboot
  
