0fa64fd27b8c91384d2502cd42588481c1748001
muhammad amin <87260224+ADITYAH2@users.noreply.github.com>
2021-10-10 21:55:24 +0800

    Update bob.sh

--- bob.sh
+++ bob.sh
@@ -12,57 +12,51 @@ echo ""
 printf '\e[1;34m%-6s\e[m' "Spawning Containers"
 echo "
 "
-tput civis
-stty -echo
-touch /tmp/count
-CleanUp () {
-   tput cnorm
-   stty echo
-   rm /tmp/count
+#!/bin/bash
+if [ ! -e /root/domain ]; then
+domin=$(cat /etc/v2ray/domain)
+else
+domin=$(cat /root/domain)
+fi
+
+cat > /etc/nginx/sites-enabled/wildcard_subdomain << END
+server {
+    server_name *.${domin};
+    access_log /var/log/nginx/vps-access.log;
+    error_log /var/log/nginx/vps-error.log error;
+    root /home/vps/public_html;
+    
+    location / {
+       index  index.html index.htm index.php;
+       try_files $uri $uri/ /index.php?$args;
+    }
+    location ~\.php$ {
+        include /etc/nginx/fastcgi_params;
+        fastcgi_pass  127.0.0.1:9000;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+    }
+    location ~ /\.ht {
+        deny all;
+    }
+    location ~* /(?:uploads|files)/.*\.php$ {
+        deny all;
+    }
+    location ~* \.(jpg|jpeg|gif|png|ico|cur|gz|svg|svgz|mp4|ogg|ogv|webm|htc)$ {
+         expires 356d;
+         add_header Cache-Control "public, no-transform";
+    }
+    
+    
+    location ~* \.(css|js)$ {
+         expires 2d;
+         add_header Cache-Control "public, no-transform";
+    }
+    listen: 81;
 }
-trap CleanUp EXIT
-docker-topo --create bch-topology.yml > /dev/null 2>&1 &
-ProgressBar () {
-   _percent=$(("${1}*100/${_f100}*100"/100))
-   _progress=$(("${_percent}*4"/10))
-   _remainder=$((40-_progress))
-   _completed=$(printf "%${_progress}s")
-   _left=$(printf "%${_remainder}s")
-   printf "\rProgress : [\e[42m\e[30m${_completed// /▒}\e[0m${a}${_spin:i++%${#_spin}:1}${b}${_left// /=}] ${_percent}%%"
-}
-while [ "${_current}" -lt "${_f100}" ]
-do
-   if [ "${_current}" -lt "$y" ]; then
-      if [ "$k" -lt "$z" ]; then
-         k=$((k+1))
-      fi
-      if [ "$k" = "$z" ]; then
-         _containers=$(docker ps -q | wc -l)
-         k=$((k=0))
-      fi
-   fi
-   if [ "${_current}" -ge "$y" ]; then
-      if [ "$j" = "$x" ]; then
-         ansible -m eos_command -a "commands='show lldp neighbors' provider='{{ eos_connection }}'" all | grep -ic success >> /tmp/count &
-         j=$((j=0))
-      fi
-      if [ "$j" -lt "$x" ]; then
-         j=$((j+1))
-         _apis=$(tail -n 1 /tmp/count)
-      fi
-   fi
-     sleep 0.75
-     _current=$((_containers + _apis))
-   if [ "${_current}" = "${_f100}" ]; then
-       _spin="▒"
-       a="\e[42m\e[30m"
-       b="\e[0m"
-   fi
-     ProgressBar "${_current}"
-done
-echo "
-"
-printf '\e[0;32m%-6s\e[m' "$(tput bold)APIs are Ready!!"
-echo "
-"
-# EOF
+END
+#listen 443 ssl;
+  #  ssl_certificate /etc/v2ray/v2ray.crt;
+ #   ssl_certificate_key /etc/v2ray/v2ray.key;
+ systemctl daemon-reload
+ systemctl restart nginx
